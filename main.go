1b7d49d11fc67d6251d8d97fcdf49ea5deb8d63e
Tran Hau <ngtranhau@gmail.com>
2020-03-22 03:39:01 +0700

    refactor to use router struct

--- main.go
+++ main.go
@@ -25,11 +25,57 @@ func main() {
 	}
 	log.Printf("PORT is %s\n", port)
 
+	names, err := getNames("names.txt")
+	if err != nil {
+		log.Println("get names failed", err)
+		return
+	}
+
+	r := router{
+		names: names,
+	}
+
+	http.HandleFunc("/", r.routeHome)
+	http.HandleFunc("/name", r.routeName)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+type router struct {
+	names []string
+}
+
+func (r *router) routeHome(w http.ResponseWriter, req *http.Request) {
+	if _, err := fmt.Fprintf(w, "GET /name\n"); err != nil {
+		log.Println(err)
+	}
+}
+
+func (r *router) routeName(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		if _, err := fmt.Fprintf(w, "wrong method %s", req.Method); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	// rand name
+	rand.Seed(time.Now().UTC().UnixNano())
+	randIndex := rand.Intn(len(r.names))
+	postFix := rand.Intn(postfixSize)
+
+	// send name
+	if _, err := fmt.Fprintf(w, "%s%d", r.names[randIndex], postFix); err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func getNames(filename string) ([]string, error) {
 	// open names.txt
 	file, err := os.Open("names.txt")
 	if err != nil {
-		log.Println("failed to open file", err)
-		return
+		return nil, err
 	}
 	log.Println("open names.txt OK")
 
@@ -45,33 +91,8 @@ func main() {
 		names = append(names, name)
 	}
 	if err := fileScanner.Err(); err != nil {
-		log.Println("failed to scan file", err)
-		return
+		return nil, err
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		if _, err := fmt.Fprintf(w, "GET /name\n"); err != nil {
-			log.Println(err)
-		}
-	})
-	http.HandleFunc("/name", func(w http.ResponseWriter, req *http.Request) {
-		if req.Method != http.MethodGet {
-			if _, err := fmt.Fprintf(w, "wrong method %s", req.Method); err != nil {
-				log.Println(err)
-				return
-			}
-		}
-
-		// rand name
-		rand.Seed(time.Now().UTC().UnixNano())
-		randIndex := rand.Intn(len(names))
-		postFix := rand.Intn(postfixSize)
-
-		if _, err := fmt.Fprintf(w, "%s%d", names[randIndex], postFix); err != nil {
-			log.Println(err)
-			return
-		}
-	})
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	return names, nil
 }
